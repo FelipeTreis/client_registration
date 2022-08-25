5774090345771fd10e9f0d9b1cda80d3129eeedf
Felipe Treis <felipetreis1@gmail.com>
2022-08-25 18:20:28 -0300

    Created pagination

--- register/views.py
+++ register/views.py
@@ -1,9 +1,15 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from register.models import Customer
 from register.serializers import CustomerSerializer
 
 
+class CustomerPagination(PageNumberPagination):
+    page_size = 1
+
+
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    pagination_class = CustomerPagination
